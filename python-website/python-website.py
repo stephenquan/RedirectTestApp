2883a555aa7f0939f309414e4ca386276c577b93
Stephen Quan <squan@esri.com>
2020-12-03 16:13:43 +1100

    Add 200ms to Python Flask server

--- python-website/python-website.py
+++ python-website/python-website.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, send_from_directory, json, jsonify, url_for
+from time import sleep
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -39,30 +40,37 @@ def request_simplify(request):
 
 @app.route('/api/debug', methods = [ 'GET', 'POST' ] )
 def api_debug():
+    sleep(0.200)
     return jsonify(request_simplify(request))
 
 @app.route('/api/debug301', methods = [ 'GET', 'POST'] )
 def api_debug301():
+    sleep(0.200)
     return redirect(url_for('api_debug', **request.args), code=301)
 
 @app.route('/api/debug302', methods = [ 'GET', 'POST'] )
 def api_debug302():
+    sleep(0.200)
     return redirect(url_for('api_debug', **request.args), code=302)
 
 @app.route('/api/debug303', methods = [ 'GET', 'POST'] )
 def api_debug303():
+    sleep(0.200)
     return redirect(url_for('api_debug', **request.args), code=303)
 
 @app.route('/api/debug305', methods = [ 'GET', 'POST'] )
 def api_debug305():
+    sleep(0.200)
     return redirect(url_for('api_debug', **request.args), code=305)
 
 @app.route('/api/debug307', methods = [ 'GET', 'POST'] )
 def api_debug307():
+    sleep(0.200)
     return redirect(url_for('api_debug', **request.args), code=307)
 
 @app.route('/api/debug308', methods = [ 'GET', 'POST'] )
 def api_debug308():
+    sleep(0.200)
     return redirect(url_for('api_debug', **request.args), code=308)
 
 if __name__ == "__main__":
